e4d07454c6bfc0bd02855d20bbbf1b45ab135c58
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-09 14:58:34 -0400

    Baseline Start with model

---
+++ BasicTrivia/Models/Questions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BasicTrivia.Models
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public string trivia { get; set; }
+        public string Answer { get; set; }
+        public string Choice1 { get; set; }
+        public string Choice2 { get; set; }
+        public string Choice3 { get; set; }
+        public string Choice4 { get; set; }
+        public string Choice5 { get; set; }        
+    }
+
+    public class QuestionDBContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+    }
+}
