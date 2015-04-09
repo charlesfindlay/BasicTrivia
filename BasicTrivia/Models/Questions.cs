20de32b27fcafcdbf4aa7874cf50a130791f1a74
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-09 15:38:52 -0400

    Added DB connection string

--- BasicTrivia/Models/Questions.cs
+++ BasicTrivia/Models/Questions.cs
@@ -9,7 +9,7 @@ namespace BasicTrivia.Models
     public class Question
     {
         public int ID { get; set; }
-        public string trivia { get; set; }
+        public string Trivia { get; set; }
         public string Answer { get; set; }
         public string Choice1 { get; set; }
         public string Choice2 { get; set; }
